214b3e02ec72b5ded6caa21773141cb6b4e7c2a1
Ihor-Hoshko <fata77698@gmail.com>
2021-12-12 19:14:58 +0200

    1

---
+++ js/01-gallery.js
@@ -0,0 +1,35 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+const galleryAll = document.querySelector(".gallery");
+
+const galleryMarkup = galleryItems
+  .map(
+    ({ preview, original, description }) => `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`
+  )
+  .join("");
+galleryAll.insertAdjacentHTML("afterbegin", galleryMarkup);
+
+gallerey.addEventListener("click", onImgClick);
+
+function onImgClick(e) {
+  e.preventDefault();
+  if (e.target.nodeName !== "IMG") {
+    return;
+  }
+}
+
+const instance = basicLightbox.create(
+  ` <div class="modal">
+    <img src="${e.target.dataset.source}" width="599" height="300" >
+    </div> `
+);
+instance.show();
